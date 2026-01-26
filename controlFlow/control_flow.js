0cfc337e23cba0d1f24f05c94f82828e9b551b98
player-001 <mohitworksop@gmail.com>
2026-01-26 15:45:32 -0500

    ternary operator

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -53,3 +53,9 @@ switch (userType){
 }
 
 console.log("User Category: ", userCategory)
+
+
+let isAuthenticated = true
+
+let authenticationStatus = isAuthenticated ? "Authenticated" : "Not Authenticated"
+console.log("Authentication Status: ", authenticationStatus)
