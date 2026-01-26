9920cfcf3820450707d83f3b555bf704c7d9e6ec
player-001 <mohitworksop@gmail.com>
2026-01-26 15:38:35 -0500

    control flow

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,55 @@
+let userRole = "admin"
+let accessLevel
+
+if (userRole == "admin"){
+    accessLevel = "Full access granted"
+}
+else if (userRole === "manager"){
+    accessLevel = "Limited access granted"
+}
+else{
+    accessLevel = "No access granted"
+}
+
+console.log("Access level: ", accessLevel)
+
+
+
+
+let isLoggedIn = true
+let userMessage
+
+if (isLoggedIn){
+    if (userRole === "admin"){
+        userMessage = "Welcome Admin"
+    }
+    else{
+        userMessage = "welcome User"
+    }
+}
+else{
+    userMessage = "Please login to access the portal"
+}
+
+console.log("User Message:", userMessage)
+
+
+
+let userType = "subscriber"
+let userCategory
+
+switch (userType){
+    case "admin":
+        userCategory = "Administrator"
+        break;
+    case "manager":
+        userCategory = "Manager"
+        break
+    case "subscriber":
+        userCategory = "Subscriber"
+        break
+    default:
+        userCategory = "Unknown"
+}
+
+console.log("User Category: ", userCategory)
