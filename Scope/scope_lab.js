bc1f175bec177f2ce212a0b41089f23281e3b77d
player-001 <mohitworksop@gmail.com>
2026-01-26 14:46:06 -0500

    scope_lab

---
+++ Scope/scope_lab.js
@@ -0,0 +1,31 @@
+// Global scope
+var globalVar = "I'm a global variable";
+let globalLet = "I'm also global, but scoped with let";
+const globalConst = "I'm a global constant";
+
+
+{
+// Block scope
+var blockVar = "I'm a block-scoped var";
+let blockLet = "I'm a block-scoped let";   //let is block bound, so beyond the block it is unaccessible
+const blockConst = "I'm a block-scoped const"; //same for const
+}
+
+console.log(globalVar)
+console.log(globalLet)
+console.log(globalConst)
+
+//console.log(blockVar) //won't work
+//console.log(blockLet)  //won't work
+
+
+function show(){
+    var functionVar = "i am a function-scoped var"
+    let functionLet = "i am a block-scoped let"
+    const functionConst = "i am a block-scoped const"
+}
+
+show()
+//console.log(functionVar)   //won't work as var is function bound
+//console.log(functionlet)   //won't work as let is block bound
+//console.log(functionConst) //won't work as const is block bound
