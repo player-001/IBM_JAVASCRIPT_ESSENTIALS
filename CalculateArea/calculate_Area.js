6521f094dbe8cce2c28eb5d876190bc993341c84
player-001 <mohitworksop@gmail.com>
2026-01-27 11:09:38 -0500

    Error Correction

--- CalculateArea/calculate_Area.js
+++ CalculateArea/calculate_Area.js
@@ -5,6 +5,6 @@ function calculateArea(){
     length = parseFloat(document.getElementById("length").value)
     width = parseFloat(document.getElementById("width").value)
 
-    let area = length * breadth
+    let area = length * width
     document.getElementById("result").innerText = `The Area of the rectangle is: ${area}`
 }
