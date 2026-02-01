bb22a1e068f902338b03b7785d5f38d4bce86958
player-001 <mohitworksop@gmail.com>
2026-02-01 15:33:45 -0500

    practice added

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    {id: 1, name: "John Doe", age: 30, department: "IT", salary: 50000},
-    {id: 2, name: "Alice Smith", age: 28, department: "HR", salary: 45000},
-    {id: 3, name: "Bob Johnson", age: 35, department: "Finance", salary: 60000}
+    {id: 1, name: "John Doe", age: 30, department: "IT", salary: 50000, specialization: "Javascript"},
+    {id: 2, name: "Alice Smith", age: 28, department: "HR", salary: 45000, specialization: "Python"},
+    {id: 3, name: "Bob Johnson", age: 35, department: "Finance", salary: 60000, specialization: "Java"}
 ]
 
 function displayEmployees(){
@@ -39,4 +39,15 @@ function findEmployeeById(employeeId){
     else{
         document.getElementById("employeesDetails").innerHTML = "No Employees Found !"
     }
+}
+
+function findBySpecialization(sub){
+    const emp = employees.filter((employee) => {
+        return employee.specialization === sub
+    })
+    const dispEmp = emp.map((e) => {
+        const s = `<p>${e.id}: ${e.name} - ${e.department} - ${e.specialization}`
+        return s
+    })
+    document.getElementById("employeesDetails").innerHTML = dispEmp
 }
